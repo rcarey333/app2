560e2375a58549c42ee45fc8bbe19cdb7741d77d
Rod Carey <rj.carey@comcast.net>
2024-07-15 23:00:05 -0400

    Initial Commit Day 21.1.3 Added data file, image files and content area.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
 
+col1, col2 = st.columns(2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+with col1:
+    st.image("images/photo.png")
 
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+    This is the bio section for Ardit.  
+    It can be updated as necessary
+    """
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    st.info(content)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# test github v2
+content2 = """
+Below you can find some of the apps I have built in Python.  Feel free to contact me!
+"""
+st.write(content2)
