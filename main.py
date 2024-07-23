5016091a2d76ce2aa288aed05754ce79a5d2e7ca
rcarey333 <126844729+rcarey333@users.noreply.github.com>
2024-07-23 15:39:47 -0400

    Day 22: Fix alignment in col3

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-         st.image("images/" + row["image"])
-         st.write(f"[Source Code]({row['url']}")
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']}")
 
 with col4:
     for index, row in df[10:].iterrows():
